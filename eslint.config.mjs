97f98c058dc46bb47ce4bce95e1ffb3eef319cb5
rammo dev <rammo@dev.com>
2025-02-14 13:23:23 +0100

    second commit, eslint rules rm

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // Remove any custom rules or overrides here
 ];
 
 export default eslintConfig;
